737f8f32db975bb9b1bbc83326156dcb7e4375e6
Hoventage <Hoventage@HOVENTAGE->
2021-01-03 11:48:49 +0300

    testCommit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        String newString = "bla-bla!11";
+//        StringBuilder stringBuilder = new StringBuilder(newString);
+//        StringBuilder ohMy = stringBuilder.delete(newString.length() - 3, newString.length());
+//
+//        System.out.println(ohMy);
+        byte testByte = 0;
+        //printBinary(testByte, 8);
+    }
+
+//    private static void printBinary(byte value, int size) {
+//        for (int i = size - 1; i >= 0; i--) {
+//            long mask = 1 << i;
+//            long result = (mask & value) >> i;
+//            System.out.print(result);
+//        }
+//        System.out.println();
+//    }
+}
