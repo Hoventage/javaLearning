6476f6f87974ca1055b15ee0f93942d529cfa7f2
Hoventage <Hoventage@HOVENTAGE->
2021-01-05 12:00:10 +0300

    before tests adding

---
+++ src/ClassToCheckOutObject.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+public class ClassToCheckOutObject {
+    int a = 10;
+    boolean bool = false;
+
+    @Override
+    public String toString() {
+        return "ClassToCheckOutObject{" +
+                "a=" + a +
+                ", bool=" + bool +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClassToCheckOutObject that = (ClassToCheckOutObject) o;
+        return a == that.a && bool == that.bool;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, bool);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,43 @@
 import java.text.MessageFormat;
+import java.lang.Object;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws CloneNotSupportedException {
 
-        BinaryPrinter testClass = new BinaryPrinter(4);
-        System.out.println(MessageFormat.format("Original testVariable is: {0}", testClass.getTestVariable()));
-        doPrint(testClass, 11);
-        //testClass.print(testClass.getTestVariable());
-        System.out.println(MessageFormat.format("Original testVariable is: {0}", testClass.getTestVariable()));
+        List<String> expectedStringList = Arrays.asList("123", "321", "12312312");
+        String oneElementFromMyList =  expectedStringList.get(2);
+        System.out.println(oneElementFromMyList);
 
-        ForExtendingTests instance = new ForExtendingTests(10);
-    }
+        /*int[] array1 = new int[100];
+        int[] array2 = {1,2,3,4,5};
 
-    private static void doPrint(BinaryPrinter printer, int value){
-        printer.print(value);
-        try {
-            printer.setTestVariable(value);
-        } catch (Exception exception){
-            System.out.println(exception.getMessage());
+        var array3 = array2.clone();
+
+        for (int a: array3) {
+            System.out.println(a);
+        }*/
+
+        /*char[] dataForString = {'a', 'b', 'c', 'd', 'e'};
+        char[] destinationCharArray = new char[dataForString.length];
+        String craftedString = new String(dataForString);
+        craftedString.getChars(0, dataForString.length, destinationCharArray, 0);
+
+        for (char ch:destinationCharArray) {
+            System.out.println(ch);
         }
+
+        String myNewStringToResearch = String.join("", craftedString);
+        System.out.println(myNewStringToResearch.charAt(3));*/
+
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < 100; i++){
+            strBuilder.append(i).append("_");
+        }
+
+        System.out.println(strBuilder);
+        System.out.println(strBuilder.toString());
+
     }
 }
